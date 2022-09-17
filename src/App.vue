e2d28c656cb2dc577dade838d357269a87571a89
owadud <akashahmed104466@gmail.com>
2022-09-17 16:44:41 +0600

    created my first component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppHeader/>
  <h1>{{title}}</h1>
  <p class="bg-red-500">I want to know you</p>
  <h1 class="text-3xl font-bold underline">
@@ -7,8 +8,10 @@
 </template>
 
 <script>
-  
+import AppHeader from './components/AppHeader.vue';
+
   export default{
+    components:{ AppHeader},
     data(){
       return { 
         title: "hey v3 checked",
