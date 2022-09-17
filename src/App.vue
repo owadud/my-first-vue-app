65e2fb49a1971cb6257ed96bb4200b0813a033d5
owadud <akashahmed104466@gmail.com>
2022-09-17 15:54:40 +0600

    tailwindcss configured

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ <h1>{{title}}</h1>
+ <p class="bg-red-500">I want to know you</p>
+ <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+  
+  export default{
+    data(){
+      return { 
+        title: "hey v3 checked",
+      }
+    }
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
