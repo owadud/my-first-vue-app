483b9e8f43dcac2ecad0b834b791a3c035e82f2a
owadud <akashahmed104466@gmail.com>
2022-09-17 22:03:05 +0600

    search bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <AppHeader/>
- <h1>{{title}}</h1>
- <p class="bg-red-500">I want to know you</p>
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+ 
 </template>
 
 <script>
@@ -12,11 +8,7 @@ import AppHeader from './components/AppHeader.vue';
 
   export default{
     components:{ AppHeader},
-    data(){
-      return { 
-        title: "hey v3 checked",
-      }
-    }
+    
   }
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,116 @@
 <template>
-  Header
+    <div class="navbar bg-base-100">
+        <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div class="flex-none gap-2">
+            <div class="form-control">
+                <form id="search-box" action="#">
+                    <label :data-state="state" for="search">
+                        <input type="text" placeholder="Search" @click="state = 'open'" @blur="state='close'" />
+                        <i class="fa fa-search" @click="true" aria-hidden="true"></i>
+                    </label>
+                </form>
+            </div>
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img src="https://placeimg.com/80/80/people" />
+                    </div>
+                </label>
+                <ul tabindex="0"
+                    class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                    <li>
+                        <a class="justify-between">
+                            Profile
+                            <span class="badge">New</span>
+                        </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+
+
 </template>
 
 <script>
 export default {
 
+    data() {
+        return {
+            state: "close"  
+        }
+
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
 
+label {
+    position: relative;
+    display: inline-block;
+    background-color: #fff;
+    padding: 5px 12px;
+    transition: all 1s ease;
+    border-radius: 0;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+
+    &::after {
+        content: "";
+        display: block;
+        height: 2px;
+        width: 80%;
+        background-color: #f0ad4e;
+        transition: all 1s ease 0.5s;
+    }
+
+    input {
+        transition: width 1s ease, opacity 0.5s ease 0.5s;
+        opacity: 1;
+        width: 180px;
+        height: 25px;
+        border: 0;
+        outline: none;
+        color: darken(#f0ad4e, 25);
+    }
+
+    i {
+        position: absolute;
+        top: 11px;
+        right: 11px;
+        color: #333;
+        cursor: pointer;
+    }
+
+    &[data-state="close"] {
+        border-radius: 30px;
+        padding: 5px 5px;
+        transition: all 1s ease;
+
+        &::after {
+            width: 0%;
+            transition: all 0.3s ease;
+        }
+
+        i {
+            pointer-events: none;
+        }
+
+        input {
+            width: 28px;
+            height: 25px;
+            opacity: 0;
+            cursor: pointer;
+            transition: opacity 0.5s ease, width 1s ease;
+            // -webkit-appearance: none;
+        }
+    }
+}
 </style>
